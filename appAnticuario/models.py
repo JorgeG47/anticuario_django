36efd9198b75e6b0d218deede113622f1bd27d4f
JorgeG47 <127901925+JorgeG47@users.noreply.github.com>
2023-06-22 13:32:47 -0400

    actualización de los archivos
    actualización de los codigos de django
    
    Co-Authored-By: Manolowo <127901712+Manolowo@users.noreply.github.com>

--- appAnticuario/models.py
+++ appAnticuario/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
+class producto(models.Model):
+    id_prod =  models.IntegerField(primary_key=True, max_length=5)
+    id_prod_etiqueta = models.CharField(max_length=15)
+   # """ id_oferta= models.ForeignKey('En oferta', on_delete=models.CASCADE, db_column='') """
+    nom_prod = models.CharField(max_length=35)
+    precio_prod = models.IntegerField(max_length=7)
+    desc_prod= models.CharField(max_length=150)
+    cantidad = models.IntegerField(max_length=3)
+
+
+
+#""" class ofertas(models.model): """
+    
